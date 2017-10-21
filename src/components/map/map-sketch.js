2be57937a45ec435f024fb5bba34e48556466049
warlyware <danward@gmail.com>
2017-10-21 16:02:43 -0400

    Add map image to screen

--- src/components/map/map-sketch.js
+++ src/components/map/map-sketch.js
@@ -4,17 +4,21 @@ export default (p5) => {
   let canvasWidth = p5.windowWidth;
   let canvasHeight = p5.windowHeight;
   let mapImage;
+  let canvas;
+  let lat = 0;
+  let long = 0;
 
   p5.preload = () => {
-    mapImage =  p5.loadImage(`https://api.mapbox.com/styles/v1/mapbox/streets-v10/static/-122.4241,37.78,14.25,0,60/600x600?access_token=${API_KEY}`);
+    mapImage =  p5.loadImage(`https://api.mapbox.com/styles/v1/mapbox/light-v9/static/0,0,1,0,0/1024x512?access_token=${API_KEY}`);
   }
 
   p5.setup = () => {
-    let canvas = p5.createCanvas(canvasWidth, canvasHeight);
+    canvas = p5.createCanvas(1024, 512);
     canvas.parent('map');
+    p5.image(mapImage, 0, 0);
   }
 
   p5.draw = () => {
-    p5.ellipse(50, 50, 80, 80);
+
   }
 }
