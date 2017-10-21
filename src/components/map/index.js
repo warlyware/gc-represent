c5e0272d8ee4c2c24da23eada7bb30d0df1ef179
warlyware <danward@gmail.com>
2017-10-21 12:55:03 -0400

    Add basic map component, add p5

---
+++ src/components/map/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Map extends Component {
+  render() {
+    return(
+      <div id="map">
+        <h1>I am map</h1>
+      </div>
+    )
+  }
+}
