e963ad60beeec6dfe0461b4b69062aaf3521df4b
warlyware <danward@gmail.com>
2017-10-21 15:20:31 -0400

    Get map image from mapbox in sketch preload method

--- src/components/map/map-sketch.js
+++ src/components/map/map-sketch.js
@@ -1,9 +1,13 @@
 import API_KEY from '../../api-key';
 
 export default (p5) => {
-  const canvasWidth = p5.windowWidth;
-  const canvasHeight = p5.windowHeight;
-  const mapImage =  p5.loadImage(`https://api.mapbox.com/styles/v1/mapbox/streets-v10/static/-122.4241,37.78,14.25,0,60/600x600?${API_KEY}`);
+  let canvasWidth = p5.windowWidth;
+  let canvasHeight = p5.windowHeight;
+  let mapImage;
+
+  p5.preload = () => {
+    mapImage =  p5.loadImage(`https://api.mapbox.com/styles/v1/mapbox/streets-v10/static/-122.4241,37.78,14.25,0,60/600x600?access_token=${API_KEY}`);
+  }
 
   p5.setup = () => {
     let canvas = p5.createCanvas(canvasWidth, canvasHeight);
