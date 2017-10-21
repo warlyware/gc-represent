41ddbcc0b375e913df20a6b95628c87eee1a36d3
warlyware <danward@gmail.com>
2017-10-21 18:01:44 -0400

    Add firebase for hosting

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ class App extends Component {
     return (
       <div className="App">
         <Helmet>
+          <title>represent! by gitconnected.com</title>
           <link href='https://api.mapbox.com/mapbox-gl-js/v0.40.0/mapbox-gl.css' rel='stylesheet' />
         </Helmet>
         <Map />
