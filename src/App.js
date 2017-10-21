94200fd8db6d14dd634da14eed5e59f0dc8cb9a2
warlyware <danward@gmail.com>
2017-10-21 15:11:08 -0400

    Add react-helmet, add css for mapbox

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Helmet } from 'react-helmet';
+
 import './App.css';
 
 import Map from './components/map';
@@ -7,6 +9,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Helmet>
+          <link href='https://api.mapbox.com/mapbox-gl-js/v0.40.0/mapbox-gl.css' rel='stylesheet' />
+        </Helmet>
         <Map />
       </div>
     );
--- src/components/map/map-sketch.js
+++ src/components/map/map-sketch.js
@@ -1,6 +1,9 @@
+import API_KEY from '../../api-key';
+
 export default (p5) => {
   const canvasWidth = p5.windowWidth;
   const canvasHeight = p5.windowHeight;
+  const mapImage =  p5.loadImage(`https://api.mapbox.com/styles/v1/mapbox/streets-v10/static/-122.4241,37.78,14.25,0,60/600x600?${API_KEY}`);
 
   p5.setup = () => {
     let canvas = p5.createCanvas(canvasWidth, canvasHeight);
